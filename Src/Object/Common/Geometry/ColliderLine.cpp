39af205efa8414382ac5b931159926ed30b3fb84
ASOST\2316032 <2316032@s.asojuku.ac.jp>
2026-04-27 12:24:20 +0900

    [fix]エラー解決 #refs .vcxprojファイルの読みこみでエラーが出ていたので削除

--- Src/Object/Common/Geometry/ColliderLine.cpp
+++ Src/Object/Common/Geometry/ColliderLine.cpp
@@ -1,9 +1,1 @@
 #include "ColliderLine.h"
-
-ColliderLine::ColliderLine(TAG tag, const Transform* follow, const VECTOR& localStartPos, const VECTOR& localEndPos)
-{
-}
-
-ColliderLine::~ColliderLine(void)
-{
-}
