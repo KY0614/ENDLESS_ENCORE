ca416505bae94b082c8588a7e60c0fc113eaf0ec
DESKTOP-EEITM3S\kakik <keikocraft87@gmail.com>
2026-07-07 14:28:37 +0900

    [add]クリアシーン用のクラス追加

--- Src/Object/Character/EventScene/ClearPlayer.h
+++ Src/Object/Character/EventScene/ClearPlayer.h
@@ -1,5 +1,8 @@
 #pragma once
+#include <functional>
 #include "../../Common/ActorBase.h"
+
+class AnimationController;
 
 class ClearPlayer : public ActorBase
 {
@@ -9,6 +12,9 @@ public:
 	enum class STATE
 	{
 		NONE,		//初期化前
+		LOOK_ENEMY,	//敵を見つめる
+		TURN,		//振り向く
+		WAIT,		//待機
 	};
 
 	//アニメーション種別
@@ -38,6 +44,37 @@ public:
 	/// 描画処理
 	/// </summary>
 	void Draw(void) override;
+
+private:
+	//アニメーション
+	std::unique_ptr<AnimationController> animationController_;
+
+	//状態管理
+	STATE state_;		//現在の状態
+
+	//状態管理(状態遷移時初期処理)
+	std::map<STATE, std::function<void(void)>> stateChanges_;
+
+	VECTOR movePow_;
+
+	/// <summary>
+	/// 3Dモデル初期化
+	/// </summary>
+	void Init3DModel(void);
+
+	/// <summary>
+	/// アニメーション初期化
+	/// </summary>
+	void InitAnimation(void);
+
+	//状態遷移
+
+	/// <summary>
+	/// 状態変更
+	/// </summary>
+	/// <param name="state">変更したい状態</param>
+	void ChangeState(const STATE& state);
+ 
 };
 
 #pragma region 流れ？
